86abba6ff6afcff099c80457fa134576e8975888
kamyarrazi14999 <kami6535@gmail.com>
2026-08-27 13:29:15 +0330

    add breakpoints for responsive design in upcoming classes swiper

--- gym/script.js
+++ gym/script.js
@@ -16,5 +16,27 @@ var swiper2 = new Swiper(".upcoming_classes_swiper", {
   autoplay: true,
   slidesPerView: 3,
   spaceBetween: 50,
-  
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+      spaceBetween:30
+      
+    },
+    1400: { 
+      slidesPerView: 2.5,
+    
+    },
+    1200: {
+      slidesPerView: 2.1,
+      
+    },
+    768: {
+      slidesPerView:1,
+      
+    },
+    500: {
+      slidesPerView:1,
+    
+    }
+  }
 });
